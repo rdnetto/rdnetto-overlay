bc7821e81c924dd41a858d54784b7822626b1914
Reuben D'Netto <rdnetto@gmail.com>
2019-04-19 18:48:03 +1000

    app-editors/neovim-qt: Fixed autobump script to exclude staging

--- app-editors/neovim-qt/autobump.sh
+++ app-editors/neovim-qt/autobump.sh
@@ -5,7 +5,12 @@ cd "$(dirname "$0")"
 CATEGORY="app-editors"
 PN="neovim-qt"
 REF_EBUILD="$PN-0.2.4.ebuild"
-LATEST="$(curl -sSfL https://api.github.com/repos/equalsraf/$PN/releases/latest | jq -r .tag_name | sed s/v//)"
+# Need to explicitly filter out the staging tag
+LATEST="$(curl -sSfL https://api.github.com/repos/equalsraf/$PN/releases \
+    | jq -r 'map(.tag_name) | .[]'         \
+    | grep -v staging                      \
+    | head -n1                             \
+    | sed s/v//)"
 
 
 if [ -e "$PN-$LATEST.ebuild" ]; then
